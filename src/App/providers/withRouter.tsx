cfb39d6edabee33bc51348562285cf726d3ed9ce
ArthurMilligan <Arthurs152@icloud.com>
2023-11-14 15:52:49 +0300

    some fix

--- src/App/providers/withRouter.tsx
+++ src/App/providers/withRouter.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const withRouter = (component: () => React.ReactNode) => () =>
   (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Suspense fallback={<div>Loading...</div>}>{component()} </Suspense>
     </BrowserRouter>
   );
