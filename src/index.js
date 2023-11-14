ea81d69c59db76c75a8c89c30b17278c93ef725d
ArthurMilligan <Arthurs152@icloud.com>
2023-11-14 15:55:20 +0300

    some fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
-
+console.log('render');
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(App()); 
+
